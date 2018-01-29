adacbea74ebc0620f84a45ecccd175012e954a34
Courtney Corlew <courtneycorlew@Courtneys-MacBook-Pro.local>
2018-01-29 13:16:20 -0500

    added log

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -19,6 +19,7 @@ app.use(require('./middleware/validate-session'));
 app.use('/api/user', require('./routes/user'));
 app.use('/api/login', require('./routes/session'));
 app.use('/api/definition', require('./routes/definition'));
+app.use('/api/log', require('./routes/log'))
 
 app.listen(3000, function(){
 	console.log('App is listening on 3000.')
