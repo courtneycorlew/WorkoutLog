5e40b882eff6b85fdc5124f338c7d3aa1aec13f1
Courtney Corlew <courtneycorlew@Courtneys-MacBook-Pro.local>
2018-01-29 14:06:01 -0500

    added delete button for log

--- workoutlogClient/workouts/log.js
+++ workoutlogClient/workouts/log.js
@@ -19,7 +19,14 @@ $(function() {
 				var len = history.length;
 				var lis = "";
 				for (var i = 0; i < len; i++) {
-					lis += "<li class='list-group-item'>" + history[i].def + " - " + history[i].result + "</li>";
+					lis += "<li class='list-group-item'>" + 
+					history[i].def + " - " + 
+					history[i].result + " " +
+					//pass the log.id into the button's id attribute // watch your quotes!
+					"<div class='pull-right'>" +
+					"<button id='" + history[i].id + "' class='update'><strong>U</strong></button>" +
+					"<button id='" + history[i].id + "' class='remove'><strong>X</strong></button>" +
+					"</div></li>"
 				}
 				$("#history-list").children().remove();
 				$("#history-list").append(lis);
@@ -45,6 +52,34 @@ $(function() {
 					  $('a[href="#history"]').tab("show");
 		      	});
 			},
+			delete: function() {
+				var thisLog = {
+					// "this" is the button on the li
+					// .attr("id") targets the value of the id attribute of button
+					id: $(this).attr("id")
+				};
+				var deleteData = { log: thisLog};
+				var deleteLog = $.ajax({
+					type: "DELETE",
+					url: WorkoutLog.API_BASE + "log",
+					data: JSON.stringify(deleteData),
+					contentType: "application/json"
+				});
+				// removes list item
+				//references button then grabs closest li
+				$(this).closest("li").remove();
+
+				//deletes item out of workouts array
+				for(var i = 0; i < WorkoutLog.log.workouts.length; i++) {
+					if(WorkoutLog.log.workouts[i].id == thisLog.id) {
+						WorkoutLog.log.workouts.splice(i,1);
+					}
+				}
+				deleteLog.fail(function() {
+					console.log("nope. you didn't delete it.");
+				});
+			},
+
 			// history
 			fetchAll: function() {
 				var fetchDefs = $.ajax({
@@ -65,6 +100,7 @@ $(function() {
 	});
 
 	$("#log-save").on("click", WorkoutLog.log.create);
+	$("#history-list").delegate('.remove', 'click', WorkoutLog.log.delete);
 
 	   // fetch history if we already are authenticated and refreshed
    if (window.localStorage.getItem("sessionToken")) {
