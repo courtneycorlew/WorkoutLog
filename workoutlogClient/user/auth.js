2b4de6df7a3c50c3a88a92079b06b5dc1766d5e9
Courtney Corlew <courtneycorlew@Courtneys-MacBook-Pro.local>
2018-01-29 11:19:13 -0500

    login/logout

--- workoutlogClient/user/auth.js
+++ workoutlogClient/user/auth.js
@@ -29,14 +29,63 @@ $(function() {
 
                 $("#signup-modal").modal("hide");
                 $(".disabled").removeClass("disabled");
-                $("#loginout").text("Logout");
+                // $("#loginout").text("Logout");
                 console.log("Great job signing up!");
 
             }).fail(function() {
                 $("#su_error").text("There was an issue with sign up").show();
             });
+        },
+        //login method 
+        login: function() {
+            //login variables
+            var username = $('#li_username').val();
+            var password = $('#li_password').val();
+            var user = {
+                user: {
+                    username: username,
+                    password: password
+                }
+            };
+
+            //login POST
+            var login = $.ajax({
+                type: "POST",
+                url: WorkoutLog.API_BASE + "login",
+                data: JSON.stringify( user ),
+                contentType: "application/json"
+            });
+
+
+            //login done/fail
+            login.done(function(data) {
+                if(data.sessionToken) {
+                    WorkoutLog.setAuthHeader(data.sessionToken);
+                    console.log(data.sessionToken)
+                }
+                $("#login-modal").modal("hide");
+                $(".disabled").removeClass("disabled");
+                $("#loginout").text("Logout");
+            }).fail(function() {
+                $("#li_error").text("There was an issue with signup").show();
+            })
+        }, 
+        loginout: function() {
+            if(window.localStorage.getItem("sessionToken")) {
+                window.localStorage.removeItem("sessionToken");
+                $("#loginout").text("Login");
+            }
+
+            //TODO: on logout make sure stuff is diabled
         }
     });
     //bind events
     $("#signup").on("click", WorkoutLog.signup);
+    $("#login").on("click", WorkoutLog.login);
+    $("#loginout").on("click", WorkoutLog.loginout);
+
+    if(window.localStorage.getItem("sessionToken")) {
+        $("#loginout").text("Logout");
+    }
+
 });
