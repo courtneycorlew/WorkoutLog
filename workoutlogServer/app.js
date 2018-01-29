61f92c3f561cb4c7fe4396f0a05b8cbae4fd9014
Courtney Corlew <courtneycorlew@Courtneys-MacBook-Pro.local>
2018-01-29 13:02:18 -0500

    added workout definitions

--- workoutlogServer/app.js
+++ workoutlogServer/app.js
@@ -1,28 +1,28 @@
 require('dotenv').config();
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db');
+
 var User = sequelize.import('./models/user');
 
-// User.sync(); // User.sync({force: true});  <--WARNING: this will DROP the table!
-sequelize.sync();
+//Create table
+User.sync(); // sync( {force: true}), to drop then create each time the app starts!
 
 app.use(bodyParser.json());
 
 app.use(require('./middleware/headers'));
 app.use(require('./middleware/validate-session'));
+
+
 app.use('/api/user', require('./routes/user'));
-//login route
 app.use('/api/login', require('./routes/session'));
-app.use('/api/definition', requite('./routes/definition'));
+app.use('/api/definition', require('./routes/definition'));
 
-app.use('/api/test', function(req,res) {
-    res.send('Hello World')
+app.listen(3000, function(){
+	console.log('App is listening on 3000.')
 });
 
-app.listen(3000, function() {
-    console.log("app is listening on 3000");
-});
 
 
